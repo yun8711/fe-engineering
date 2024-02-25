dd5e322065ff37113eaac54b147695a806f725ec
liuyun <liuyun.dev@qq.com>
2024-02-25 17:11:42 +0800

    feat(文档): 权限系统

--- docs/.vitepress/config.general.mjs
+++ docs/.vitepress/config.general.mjs
@@ -3,6 +3,7 @@ export default [
     items: [
       { text: "文本格式", link: "/general/text-format" },
       { text: "身份认证", link: "/general/authentication" },
+      { text: "权限系统&模型", link: "/general/authorization" },
     ],
   },
 ];
